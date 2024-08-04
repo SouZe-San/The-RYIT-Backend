be49cf5c3693c3c1ff7264988c3aa540c742533e
SouZe-San <2003soumyajit@gamil.com>
2024-08-04 11:20:38 +0530

    course Schema Added

--- schemaTypes/course.js
+++ schemaTypes/course.js
@@ -6,8 +6,8 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
+      name: 'courseName',
+      title: 'Course Name',
       type: 'string',
       validation: (Rule) => Rule.required(),
     }),
@@ -23,18 +23,28 @@ export default defineType({
     defineField({
       name: 'courseImage',
       title: 'Course image',
+      description: 'Please provide a Poster image for this course',
       type: 'image',
       options: {hotspot: true},
-    }),
-    defineField({
-      name: 'description',
-      title: 'Course Description',
-      type: 'string',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'categories',
-      title: 'Categories',
+      name: 'shortDescription',
+      title: 'Course Short Description',
+      description: 'Please provide a short description of the course',
+      type: 'text',
+      validation: (Rule) => Rule.required().max(150).error('Not more than 20 words'),
+    }),
+    defineField({
+      name: 'fullDescription',
+      title: 'Course full details',
+      description: 'Give a Full description of the course',
+      type: 'text',
+      validation: (Rule) => Rule.required().max(500).error('Not more than 100 words'),
+    }),
+    defineField({
+      name: 'CourseLevel',
+      title: 'Course level',
       type: 'string',
       options: {
         list: [
@@ -46,14 +56,48 @@ export default defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'price',
-      title: 'Price (INR)',
-      type: 'number',
+      name: 'CourseDuration',
+      title: 'Course Duration',
+      type: 'string',
+      options: {
+        list: [
+          {title: '1 month', value: '1 month'},
+          {title: '2 month', value: '2 month'},
+          {title: '3 month', value: '3 month'},
+          {title: '6 month', value: '6 month'},
+          {title: '1 year', value: '1 year'},
+          {title: '2 year', value: '2 year'},
+        ],
+      },
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'duration',
-      title: 'Duration (in months)',
-      type: 'number',
+      name: 'courseEligibility',
+      title: 'Course Eligibility',
+      description: 'Who is this course for / Who can take this course',
+      type: 'text',
+      validation: (Rule) => Rule.required().max(300).error('Not more than 60 words'),
+    }),
+    defineField({
+      name: 'courseOpportunities',
+      title: 'Course opportunities',
+      description: 'Please provide the opportunities after the course',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+    defineField({
+      name: 'extraFacilities',
+      title: 'Extra Facilities',
+      description: 'What are the extra facilities provided with the course',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+    defineField({
+      name: 'courseLessons',
+      title: 'Course Content',
+      description: 'Please provide the lessons of the course',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
   ],
 })
